2a6e5ba4295f54d006f9a806286e98f9d833a931
Cassidy Tarng <catarng@cisco.com>
2018-07-18 14:40:55 -0700

    quickSor

---
+++ src/SortAlgorithms/QuickSort.java
@@ -0,0 +1,49 @@
+package SortAlgorithms;
+
+public class QuickSort {
+
+    public static void main(String[] args){
+        int[] arr = {4, 3, 2, 1, 5, 33, 5};
+
+        quickSort(0, arr.length - 1, arr);
+
+        for(int i : arr){
+            System.out.println(i);
+        }
+    }
+
+    public static void quickSort(int left, int right, int[] arr){
+        if (left < right){
+            int mid = partition(left, right, arr);
+
+            quickSort(left, mid - 1, arr);
+            quickSort(mid + 1, right, arr);
+        }
+    }
+
+    public static int partition(int left, int right, int[] arr){
+        int pivot = arr[right];
+
+        // place first pointer -1 below start
+        int i = left - 1;
+
+        for (int k = left; k < right; k++){
+            // if current point is less than pivot
+            if (arr[k] < pivot){
+
+                // increment i and swap with k
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[k];
+                arr[k] = temp;
+            }
+        }
+
+        // swap pivot with i + 1
+        arr[right] = arr[i + 1];
+        arr[i + 1] = pivot;
+
+        return i + 1;
+    }
+
+}
