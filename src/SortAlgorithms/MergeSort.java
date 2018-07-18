cf5e8c230d6d942dd61bc98c04743bee65c264ba
Cassidy Tarng <catarng@cisco.com>
2018-07-18 14:23:19 -0700

    practiced mergesort

--- src/SortAlgorithms/MergeSort.java
+++ src/SortAlgorithms/MergeSort.java
@@ -71,4 +71,57 @@ public class MergeSort {
         }
 
     }
+
+//    public static void mergeSort(int left, int right, int[] arr){
+//        if (left < right){
+//            int mid = (left + right) / 2;
+//            mergeSort(left, mid, arr);
+//            mergeSort(mid + 1, right, arr);
+//
+//            merge(left, mid, right, arr);
+//        }
+//
+//    }
+//
+//    public static void merge(int left, int mid, int right, int[] arr){
+//        int leftSize = mid - left + 1;
+//        int rightSize = right - mid;
+//
+//        int[] L = new int[leftSize];
+//        int[] R = new int[rightSize];
+//
+//        for(int i = 0; i < leftSize; i++){
+//            L[i] = arr[left + i];
+//        }
+//        for(int i = 0; i < rightSize; i++){
+//            R[i] = arr[mid + 1 + i];
+//        }
+//
+//        int i = 0;
+//        int k = 0;
+//        int count = left;
+//
+//        while (i < leftSize && k < rightSize){
+//            if (L[i] < R[k]){
+//                arr[count] = L[i];
+//                i++;
+//                count++;
+//            }
+//            else{
+//                arr[count] = R[k];
+//                k++;
+//                count++;
+//            }
+//        }
+//        while (i < leftSize){
+//            arr[count] = L[i];
+//            i++;
+//            count++;
+//        }
+//        while (k < rightSize){
+//            arr[count] = R[k];
+//            k++;
+//            count++;
+//        }
+//    }
 }
